8f17f94c3508010105ff30a2746eab7e9359d6dc
Maksym Yarema <macsym2@gmail.com>
2023-06-02 10:41:03 +0300

    Start

---
+++ src/main/java/com/maks/puzzlesproject/PuzzlesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maks.puzzlesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maks/puzzlesproject/PuzzlesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maks.puzzlesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
